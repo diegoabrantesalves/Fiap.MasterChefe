3eb728b28a3ceb6fc01e6a21eaa0b8d1da3bf71b
Alexandre de Oliveira Novaes <alex.novaes8@gmail.com>
2017-10-08 03:26:18 -0300

    Implementação do controller CategoriaReceita

--- Fiap.MasterChefe.Infra.CrossCutting.IoC/NativeInjector.cs
+++ Fiap.MasterChefe.Infra.CrossCutting.IoC/NativeInjector.cs
@@ -15,16 +15,19 @@ namespace Fiap.MasterChefe.Infra.CrossCutting.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
-
             //Aplicação
             services.AddSingleton(Mapper.Configuration);
             services.AddScoped<IMapper>(sp => new Mapper(sp.GetRequiredService<IConfigurationProvider>(), sp.GetService));
+            services.AddScoped<ICategoriaReceitaAppService, CategoriaReceitaAppService>();
             services.AddScoped<IReceitaAppService, ReceitaAppService>();
+            services.AddScoped<IIngredienteAppService, IngredienteAppService>();
+            services.AddScoped<IModoDePreparoAppService, ModoDePreparoAppService>();
 
-            
-            
             //Infra
+            services.AddScoped<ICategoriaReceitaRepositorio, CategoriaReceitaRepositorio>();
             services.AddScoped<IReceitaRepositorio, ReceitaRepositorio>();
+            services.AddScoped<IIngredienteRepositorio, IngredienteRepositorio>();
+            services.AddScoped<IModoDePreparoRepositorio, ModoDePreparoRepositorio>();
             services.AddScoped<Contexto>();
         }
     }
---
+++ Fiap.MasterChefe.Web/Controllers/CategoriasReceitaController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Fiap.MasterChefe.Aplicacao.Interfaces;
+using Fiap.MasterChefe.Aplicacao.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.MasterChefe.Web.Controllers
+{
+    public class CategoriasReceitaController : Controller
+    {
+        private ICategoriaReceitaAppService _categoriaReceitaAppService;
+
+        public CategoriasReceitaController(ICategoriaReceitaAppService categoriaReceitaAppService)
+        {
+            _categoriaReceitaAppService = categoriaReceitaAppService;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_categoriaReceitaAppService.GetAll());
+        }
+
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var model = _categoriaReceitaAppService.GetById(id.Value);
+            if (model == null)
+                return NotFound();
+
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("Id,Descricao,TempodePreparo,Rendimento,MododePreparo")] CategoriaReceitaViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = Guid.NewGuid();
+                _categoriaReceitaAppService.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var model = _categoriaReceitaAppService.GetById(id.Value);
+            if (model == null)
+                return NotFound();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, [Bind("Id,Descricao,TempodePreparo,Rendimento,MododePreparo")] CategoriaReceitaViewModel model)
+        {
+            if (id != model.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _categoriaReceitaAppService.Update(model);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriaReceitaViewModelExists(model.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var model = _categoriaReceitaAppService.GetById(id.Value);
+            if (model == null)
+                return NotFound();
+
+            return View(model);
+        }
+
+        // POST: Receitas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(Guid id)
+        {
+            var model = _categoriaReceitaAppService.GetById(id);
+            _categoriaReceitaAppService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoriaReceitaViewModelExists(Guid id)
+        {
+            return _categoriaReceitaAppService.GetById(id) != null;
+        }
+    }
+}
